6b6d02ba2ba448eedc362ac42a47a91aa12531fa
Fahnamanahm <john.pologruto@gmail.com>
2025-12-02 17:01:15 -0800

    Initial Commit

---
+++ ChartAnything/ChartAnythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartAnythingApp.swift
+//  ChartAnything
+//
+//  Created by John Pologruto on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartAnythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
